aba6a9f427a5d910d9b59fca342c0c7c6cdf3078
Kame-Bashuna <basshunakameroba@gmail.com>
2024-03-19 16:25:12 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main(){
+    var c=12
+    var d=34
+    println(c+d)
+    println(c-d)
+    println(c*d)
+    println(c/d)
+
+    var width=45
+    var length=30
+    var area=width*length
+    println(area)
+
+    var celisius=20
+    var fahrenheit=celisius*9/5+32
+    println("The temperature in Fahrenheit"+ " "+fahrenheit)
+
+    var name="kame"
+    var age=50
+    val favoritecolor="red"
+    println((name)+" "+(age)+" "+(favoritecolor))
+
+    var num1=31
+    var num2=21
+    var num3=30
+    var average=num1+num2+num3
+    println(average/3)
+
+    var fullName="zipy"
+    var age1=21
+    var phoneNumber="0754321356"
+    var weight=45.6
+    var citizen="kenyan"
+    println(citizen)
+
+
+
+
+
+}
